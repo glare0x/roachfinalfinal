1121fcdb70f6908e24d5a958628f4ca6639b04d8
glare <dirtbik3r999@gmail.com>
2024-03-22 09:47:37 -0400

    fixed3

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: { manifest: true, outDir: './dist'},
+  base:  '/',
+  root: './src',
   plugins: [react()],
 })
